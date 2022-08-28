4aedcf52b6f0de74ffcadce4c925e1fe12b15bf5
Harendra Dilshan <90233508+HarendraD@users.noreply.github.com>
2022-08-28 21:27:45 +0530

    Project Added

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 4000;
+app.use(express.json());
+const users = require("./routes/users/users")
+const posts = require("./routes/posts/posts")
+
+const url = "mongodb://127.0.0.1/facebookDb";
+mongoose.connect(url,{useNewUrlParser:true});
+const con = mongoose.connection;
+
+con.on("open",()=>{
+    console.log("DataBase Connected");
+})
+
+app.use("/users",users);
+app.use("/posts",posts);
+
+app.listen(port,()=>{
+    console.log("App Running");
+})
